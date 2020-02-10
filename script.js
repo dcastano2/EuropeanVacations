a3228c5a814a5f45b0892c53add1a9ea73e23f95
dcastano2 <47287214+dcastano2@users.noreply.github.com>
2020-02-09 19:45:28 -0800

    updated the bandtemplate to b/w & added personal pic

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-function multiply(a, b) {
-	return a * b;
-}
+var database = [
+	{
+		username: "andrei",
+		password: "supersecret"
+	}
+];
 
-multiply(5,10);
+var newsFeed = [
+	{
+		username:  "Bobby",
+		timeline:  "So tried from all that learning"
+	}, 
+	{
+		username: "Sally",
+		timeline: "Javascript is sooo cool" 
+	}
+
+
+];
+
+var userNamePrompt = prompt("what is your username?");
+var passwordPrompt = prompt("what is your password?");
+
+function signIn(user, pass) {
+	if (user === database[0].username && pass === database[0].password){
+		console.log(newsFeed);
+	}
+	else {
+		alert("Sorry, wrong username and password");
+	}
+}
+signIn(userNamePrompt, passwordPrompt);
